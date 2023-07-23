a927bc9e8d0eddb531ec7298b0344229c480b56b
“133943217-myseneca” <mashaikh10@myseneca.ca>
2023-07-22 22:06:37 -0400

    updated pre processing logic

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -200,7 +200,7 @@ latlong=lat:long'
  
      identifier = "IoT device performance and failures"
 
-     preprocesslogic='anomprob,trend,avg'
+     preprocesslogic='anomprob,trend,sum,variance,anomprob'
 
      
 #     pathtotmlattrs='oem=id,lat=subject.reference,long=component.0.code.coding.0.display,location=component.1.valueQuantity.value'     
