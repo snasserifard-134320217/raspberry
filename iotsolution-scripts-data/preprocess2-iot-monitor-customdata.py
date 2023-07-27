ddfd3c1410312757e6008e6215e471010a614217
“133943217-myseneca” <mashaikh10@myseneca.ca>
2023-07-26 20:43:37 -0400

    changed preprocess logic in preprocess2 file

--- iotsolution-scripts-data/preprocess2-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess2-iot-monitor-customdata.py
@@ -138,7 +138,7 @@ def sendtransactiondata(maintopic,mainproducerid,VIPERPORT,index,preprocesstopic
       # You can preprocess with the following functions: MAX, MIN, SUM, AVG, COUNT, DIFF
       # here we will take max values of the arcturus-humidity, we will Diff arcturus-temperature, and average arcturus-Light_Intensity
       # NOTE: The number of process logic functions MUST match the streams - the operations will be applied in the same order
-     preprocesslogic='sum,sum,sum,sum'
+     preprocesslogic='avg,avg,avg,avg'
      #preprocesslogic='diff'
      preprocessconditions=''
     
